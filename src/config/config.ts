d4f3ebcd14dfc0b84440edfe63f9ec574ba25433
Thiago Barroso <thiago.barroso@somagrupo.com.br>
2025-02-10 21:19:06 -0300

    meta

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,11 +1,11 @@
 import 'dotenv/config'
 
 export const firebaseConfig = {
-  databaseURL: process.env.databaseURL,
-  apiKey: process.env.apiKey, 
-  authDomain: process.env.authDomain, 
-  projectId: process.env.projectId, 
-  storageBucket: process.env.storageBucket, 
-  messagingSenderId: process.env.messagingSenderId, 
-  appId: process.env.appId
+  databaseURL: import.meta.env.databaseURL || process.env.databaseURL,
+  apiKey: import.meta.env.apiKey || process.env.apiKey, 
+  authDomain: import.meta.env.authDomain || process.env.authDomain, 
+  projectId: import.meta.env.projectId || process.env.projectId, 
+  storageBucket: import.meta.env.storageBucket || process.env.storageBucket, 
+  messagingSenderId: import.meta.env.messagingSenderId || process.env.messagingSenderId, 
+  appId: import.meta.env.appId || process.env.appId
 };
